1c7334dd72a0597ab39bab090c64051f8bf951c8
sean <thechis@gmail.com>
2021-08-30 00:10:43 +0100

    added to ignore

--- app/routes.py
+++ app/routes.py
@@ -11,8 +11,8 @@ def index():
     posts = Post.query.order_by(Post.id).all()
     return render_template('index.html', title='Home Page', posts=posts)
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
+@app.route('/a', methods=['GET', 'POST'])
+def a():
     if current_user.is_authenticated:
         return redirect(url_for('index'))
     form = LoginForm()
@@ -20,13 +20,13 @@ def login():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
             flash('Invalid username or password')
-            return redirect(url_for('login'))
+            return redirect(url_for('a'))
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('index')
         return redirect(next_page)
-    return render_template('login.html', title='Sign In', form=form)
+    return render_template('a.html', title='Sign In', form=form)
 
 @app.route('/logout')
 def logout():
