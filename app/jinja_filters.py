e5582cafb53fb43695cd2a2cb5c61a166849c5da
sean <thechis@gmail.com>
2021-09-02 23:54:32 +0100

    minor changes

--- app/jinja_filters.py
+++ app/jinja_filters.py
@@ -5,6 +5,6 @@ In order to pass custom functions through jinja2 templates,
 they are defined here and set in __init__.py
 """
 
-def datetime_format(value, format="%d-%m-%y"):
+def datetime_format(value, format="%B %d %Y"):
     return value.strftime(format)
 
